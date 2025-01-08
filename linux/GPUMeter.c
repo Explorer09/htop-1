8ec48bcbb52ae78d1fcc12285bbf98be62152e26
Explorer09 <explorer09@gmail.com>
2025-01-09 03:21:03 +0800

    humanTimeUnit revert3

--- linux/GPUMeter.c
+++ linux/GPUMeter.c
@@ -50,7 +50,7 @@ static int humanTimeUnit(char* buffer, size_t size, unsigned long long totalNano
    value /= 10; // microseconds
 
    if (value < 10000)
-      return xSnprintf(buffer, size, "%4uus", (unsigned int)value);
+      return xSnprintf(buffer, size, "%4lluus", value);
 
    value /= 100;
 
