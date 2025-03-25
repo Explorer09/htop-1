htop-dev/htop

commit 7fdd8d3732354b3fcca2d0430731e8e33345ddda

adding support for more than 2 smaller cpumeter columns

CPUMeter.c
@@ -137,6 +137,15 @@ static void AllCPUsMeter_getRange(Meter* this, int* start, int* count) {
    }
 }
 
+static int MapClassnameToColumncount(Meter* this){
+   if (strchr(Meter_name(this), '4'))
+      return 4;
+   else if (strchr(Meter_name(this), '2'))
+      return 2;
+   else
+      return 1;
+}
+
 static void AllCPUsMeter_init(Meter* this) {
    int cpus = this->pl->cpuCount;
    if (!this->drawData)
@@ -152,10 +161,8 @@ static void AllCPUsMeter_init(Meter* this) {
    if (this->mode == 0)
       this->mode = BAR_METERMODE;
    int h = Meter_modes[this->mode]->h;
-   if (strchr(Meter_name(this), '2'))
-      this->h = h * ((count+1) / 2);
-   else
-      this->h = h * count;
+   int ncol = MapClassnameToColumncount(this);
+   this->h = h * ((count + ncol - 1)/ ncol);
 }
 
 static void AllCPUsMeter_done(Meter* this) {
@@ -175,10 +182,8 @@ static void AllCPUsMeter_updateMode(Meter* this, int mode) {
    for (int i = 0; i < count; i++) {
       Meter_setMode(meters[i], mode);
    }
-   if (strchr(Meter_name(this), '2'))
-      this->h = h * ((count+1) / 2);
-   else
-      this->h = h * count;
+   int ncol = MapClassnameToColumncount(this);
+   this->h = h * ((count + ncol - 1)/ ncol);
 }
 
 static void DualColCPUsMeter_draw(Meter* this, int x, int y, int w) {
@@ -207,6 +212,21 @@ static void SingleColCPUsMeter_draw(Meter* this, int x, int y, int w) {
       meters[i]->draw(meters[i], x, y, w);
       y += meters[i]->h;
    }
+}
+
+static void MultiColCPUsMeter_draw(Meter* this, int x, int y, int w){
+  Meter** meters = (Meter**) this->drawData;
+  int start, count;
+  AllCPUsMeter_getRange(this, &start, &count);
+  int ncol = MapClassnameToColumncount(this);
+  int colwidth = (w-ncol)/ncol + 1;
+  int diff = (w - (colwidth * ncol));
+  for (int i = 0; i < count; i++){
+    int d = (i%ncol) > diff ? diff : (i % ncol) ; // dynamic spacer
+    int xpos = x + ((i % ncol) * colwidth) + d;
+    int ypos = y + ((i / ncol) * meters[0]->h);
+    meters[i]->draw(meters[i], xpos, ypos, colwidth);
+  }
 }
 
 MeterClass CPUMeter_class = {
@@ -340,3 +360,59 @@ MeterClass RightCPUs2Meter_class = {
    .done = AllCPUsMeter_done
 };
 
+MeterClass AllCPUs4Meter_class = {
+   .super = {
+      .extends = Class(Meter),
+      .delete = Meter_delete,
+      .display = CPUMeter_display
+   },
+   .defaultMode = CUSTOM_METERMODE,
+   .total = 100.0,
+   .attributes = CPUMeter_attributes,
+   .name = "AllCPUs4",
+   .uiName = "CPUs (1&2&3&4/4)",
+   .description = "CPUs (1&2&3&4/4): all CPUs in 4 shorter columns",
+   .caption = "CPU",
+   .draw = MultiColCPUsMeter_draw,
+   .init = AllCPUsMeter_init,
+   .updateMode = AllCPUsMeter_updateMode,
+   .done = AllCPUsMeter_done
+};
+
+MeterClass LeftCPUs4Meter_class = {
+   .super = {
+      .extends = Class(Meter),
+      .delete = Meter_delete,
+      .display = CPUMeter_display
+   },
+   .defaultMode = CUSTOM_METERMODE,
+   .total = 100.0,
+   .attributes = CPUMeter_attributes,
+   .name = "LeftCPUs4",
+   .uiName = "CPUs (1-4/8)",
+   .description = "CPUs (1-4/8): first half in 4 shorter columns",
+   .caption = "CPU",
+   .draw = MultiColCPUsMeter_draw,
+   .init = AllCPUsMeter_init,
+   .updateMode = AllCPUsMeter_updateMode,
+   .done = AllCPUsMeter_done
+};
+
+MeterClass RightCPUs4Meter_class = {
+   .super = {
+      .extends = Class(Meter),
+      .delete = Meter_delete,
+      .display = CPUMeter_display
+   },
+   .defaultMode = CUSTOM_METERMODE,
+   .total = 100.0,
+   .attributes = CPUMeter_attributes,
+   .name = "RightCPUs4",
+   .uiName = "CPUs (5-8/8)",
+   .description = "CPUs (5-8/8): second half in 4 shorter columns",
+   .caption = "CPU",
+   .draw = MultiColCPUsMeter_draw,
+   .init = AllCPUsMeter_init,
+   .updateMode = AllCPUsMeter_updateMode,
+   .done = AllCPUsMeter_done
+};

CPUMeter.h
@@ -47,5 +47,10 @@ extern MeterClass LeftCPUs2Meter_class;
 
 extern MeterClass RightCPUs2Meter_class;
 
+extern MeterClass AllCPUs4Meter_class;
+
+extern MeterClass LeftCPUs4Meter_class;
+
+extern MeterClass RightCPUs4Meter_class;
 
 #endif

linux/Platform.c
@@ -121,10 +121,13 @@ MeterClass* Platform_meterTypes[] = {
    &HostnameMeter_class,
    &AllCPUsMeter_class,
    &AllCPUs2Meter_class,
+   &AllCPUs4Meter_class,
    &LeftCPUsMeter_class,
    &RightCPUsMeter_class,
    &LeftCPUs2Meter_class,
    &RightCPUs2Meter_class,
+   &LeftCPUs4Meter_class,
+   &RightCPUs4Meter_class,
    &BlankMeter_class,
    NULL
 };

Explorer09/htop-1

commit b4ef5608447a4cebb23dda6b8102ac9e488f35cf

CPUMeter: Fix negative "x" positions of sub-meters
A formula error in CPUMeterCommonDraw() can cause some CPU sub-meters to
have negative "x" positions when drawing. (The negative "x" can happen
when the terminal screen width is very small.) This can cause assertion
errors in the meters' draw() functions.

Fix the formula of calculating "colwidth" in the function. Note the
"colwidth" value (the terminal width of a sub-meter column) is now
allowed to be zero.

(Regression from commit 7fdd8d3732354b3fcca2d0430731e8e33345ddda)

Signed-off-by: Kang-Che Sung <explorer09@gmail.com>

CPUMeter.c
@@ -305,8 +305,8 @@ static void CPUMeterCommonDraw(Meter* this, int x, int y, int w, int ncol) {
    Meter** meters = data->meters;
    int start, count;
    AllCPUsMeter_getRange(this, &start, &count);
-   int colwidth = (w - ncol) / ncol + 1;
-   int diff = (w - (colwidth * ncol));
+   int colwidth = w / ncol;
+   int diff = w % ncol;
    int nrows = (count + ncol - 1) / ncol;
    for (int i = 0; i < count; i++) {
       int d = (i / nrows) > diff ? diff : (i / nrows); // dynamic spacer

MemorySwapMeter.c
@@ -38,7 +38,7 @@ static void MemorySwapMeter_draw(Meter* this, int x, int y, int w) {
 
    /* Use the same width for each sub meter to align with CPU meter */
    const int colwidth = w / 2;
-   const int diff = w - colwidth * 2;
+   const int diff = w % 2;
 
    assert(data->memoryMeter->draw);
    data->memoryMeter->draw(data->memoryMeter, x, y, colwidth);