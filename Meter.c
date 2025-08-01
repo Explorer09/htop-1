0d3072a78444029e39e4062fb52b04ca452eb402
Explorer09 <explorer09@gmail.com>
2025-08-01 20:16:39 +0800

    tweak 2b

--- Meter.c
+++ Meter.c
@@ -1000,7 +1000,7 @@ static int GraphMeterMode_lookupCell(const Meter* this, const GraphDrawContext* 
 
    unsigned int numDots = valueStart[1].numDots;
    if (numDots > 1) {
-      if (deltaExp + 1 < UINT16_WIDTH) {
+      if (deltaExp < UINT16_WIDTH) {
          numDots = ((numDots - 1) >> 1 >> deltaExp) + 1;
       } else {
          numDots = 1;
@@ -1010,7 +1010,7 @@ static int GraphMeterMode_lookupCell(const Meter* this, const GraphDrawContext* 
 
    numDots = valueStart[2].numDots;
    if (numDots > 1) {
-      if (deltaExp + 1 < UINT16_WIDTH) {
+      if (deltaExp < UINT16_WIDTH) {
          numDots = ((numDots - 1) >> 1 >> deltaExp) + 1;
       } else {
          numDots = 1;
