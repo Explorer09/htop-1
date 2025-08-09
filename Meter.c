d121c17a1183a589483b6db57de77d37b8e34319
Explorer09 <explorer09@gmail.com>
2025-08-10 05:19:18 +0800

    v9h: nCellsPerValue

--- Meter.c
+++ Meter.c
@@ -1310,11 +1310,9 @@ static void GraphMeterMode_draw(Meter* this, int x, int y, int w) {
 
    bool isPercentChart = Meter_isPercentChart(this);
 
-   size_t nCellsPerValue;
+   size_t nCellsPerValue = h;
    if (this->mode == GRAPH2_METERMODE || maxItems == 1) {
       nCellsPerValue = maxItems;
-   } else {
-      nCellsPerValue = h;
    }
    if (!isPercentChart) {
       nCellsPerValue *= 2;
