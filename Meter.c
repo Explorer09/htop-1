9017cec262ee49ed96a21c6f11f0919aa0689288
Explorer09 <explorer09@gmail.com>
2025-08-17 06:20:12 +0800

    v13f

--- Meter.c
+++ Meter.c
@@ -1143,7 +1143,7 @@ static int GraphMeterMode_lookupCell(const Meter* this, const GraphDrawContext* 
       }
 
       itemIndex = 0;
-      if (maxItems == 2) {
+      if (this->mode == GRAPH2_METERMODE) {
          if (y * 2 < h - 1) {
             itemIndex = 1;
          } else if (y * 2 == h - 1 && secondItemLarger) {
