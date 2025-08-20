0cdb16d05064a53c3a6be3fe44d783ceee0a4d21
Explorer09 <explorer09@gmail.com>
2025-08-21 03:27:53 +0800

    tm7d2

--- Meter.c
+++ Meter.c
@@ -938,7 +938,7 @@ static void GraphMeterMode_recordNewValue(Meter* this, const GraphDrawContext* c
       memset(&valueStart[i], 0, (nCellsPerValue - i) * sizeof(*valueStart));
    }
 
-   if (sum <= 0.0) {
+   if (numDots <= 0) {
       // The record is empty. No colors needed.
       return;
    }
