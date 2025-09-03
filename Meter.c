142c34b26feae6388470f61b3ebeb6da866bfd8f
Explorer09 <explorer09@gmail.com>
2025-09-04 05:51:25 +0800

    u1k3

--- Meter.c
+++ Meter.c
@@ -942,7 +942,7 @@ static void GraphMeterMode_recordNewValue(Meter* this, const GraphDrawContext* c
    while (true) {
       GraphMeterMode_computeColors(this, context, valueStart, deltaExp, scaledTotal, numDots);
 
-      if (isPercentChart || (2U << deltaExp) >= h * 2 || !(scaledTotal < DBL_MAX))
+      if (isPercentChart || (2U << deltaExp) > h * 2 - 1 || !(scaledTotal < DBL_MAX))
          break;
 
       deltaExp++;
