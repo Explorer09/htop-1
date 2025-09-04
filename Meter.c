2e43c19aca85e0ca7f7a0b8ae63b0a1b3e493f4e
Explorer09 <explorer09@gmail.com>
2025-09-04 20:38:08 +0800

    u1L

--- Meter.c
+++ Meter.c
@@ -607,7 +607,7 @@ static void GraphMeterMode_computeColors(Meter* this, const GraphDrawContext* co
    bool hasPartialTopCell = false;
    if (blanksAtTopCell > 0) {
       hasPartialTopCell = true;
-   } else if (!isPercentChart && topCell % 2 == 0 && ((topCell + 1) << deltaExp) >= h) {
+   } else if (!isPercentChart && ((topCell | 1) << deltaExp) >= h) {
       // This "top cell" is rendered as full in one scale, but partial in the
       // next scale. (Only happens when "h" is not a power of two.)
       hasPartialTopCell = true;
