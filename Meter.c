301ab3ea6a5dc55a02ac0c16a27da016b13fc882
Explorer09 <explorer09@gmail.com>
2025-09-13 03:05:20 +0800

    wd2a

--- Meter.c
+++ Meter.c
@@ -937,15 +937,11 @@ static void GraphMeterMode_recordNewValue(Meter* this, const GraphDrawContext* c
       }
       assert(numDots <= UINT16_MAX - (8 - 1));
 
-      if (maxItems == 1 || this->mode == GRAPH2_METERMODE) {
-         // We just need to record the number of dots in the graph data buffer.
-         itemStart[itemIndex].numDots = (uint16_t)numDots;
-      }
-
-      if (!(maxItems == 1 || this->mode == GRAPH2_METERMODE)) {
-         // simplify this to (this->mode != GRAPH2_METERMODE) ?
+      if (!(maxItems == 1 || this->mode == GRAPH2_METERMODE))
          break;
-      }
+
+      // We just need to record the number of dots in the graph data buffer.
+      itemStart[itemIndex].numDots = (uint16_t)numDots;
    } while (++itemIndex < maxItems);
    if (maxItems == 1 || this->mode == GRAPH2_METERMODE) {
       return;
