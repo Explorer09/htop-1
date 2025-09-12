190d22e85fcefd3736679252d4ec3a6df6482d89
Explorer09 <explorer09@gmail.com>
2025-09-13 03:05:20 +0800

    wd5

--- Meter.c
+++ Meter.c
@@ -919,13 +919,13 @@ static void GraphMeterMode_recordNewValue(Meter* this, const GraphDrawContext* c
 
    unsigned int numDots = 0;
    uint8_t itemIndex = 0;
+   double value = sum;
    GraphDataCell* itemStart = &valueStart[isPercentChart ? 0 : 1];
    while (true) {
       numDots = 0;
       if (total > 0.0) {
-         double value = sum;
-         if ((maxItems == 1 || this->mode == GRAPH2_METERMODE) && itemIndex < this->curItems) {
-            value = this->values[itemIndex];
+         if (this->mode == GRAPH2_METERMODE) {
+            value = itemIndex < this->curItems ? this->values[itemIndex] : 0.0;
          }
          if (isPositive(value)) {
             value = MINIMUM(total, value); // Clamp when value is infinity
