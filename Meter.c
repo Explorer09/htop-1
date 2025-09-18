215bd97f0ab4641d582465fcc6f0cbb118966651
Explorer09 <explorer09@gmail.com>
2025-09-19 05:04:24 +0800

    wc8c (disasm)

--- Meter.c
+++ Meter.c
@@ -879,6 +879,8 @@ static void GraphMeterMode_recordNewValue(Meter* this, const GraphDrawContext* c
       assert(sum <= DBL_MAX);
    }
 
+   bool storeNumDots = maxItems == 1 || this->mode == GRAPH2_METERMODE;
+
    // "total" refers to the value that we would draw as full in graph
    double total = sum;
    if (isPercentChart) {
@@ -888,7 +890,7 @@ static void GraphMeterMode_recordNewValue(Meter* this, const GraphDrawContext* c
       // Determine the scale and "total" that we need afterward. The "total" is
       // rounded up to a power of 2.
 
-      if (this->mode == GRAPH2_METERMODE) {
+      if (storeNumDots) {
          // Find the greatest value in this->values array
          for (uint8_t i = 0; i < maxItems && i < this->curItems; i++) {
             if (isgreater(this->values[i], total)) {
@@ -920,7 +922,6 @@ static void GraphMeterMode_recordNewValue(Meter* this, const GraphDrawContext* c
    unsigned int numDots = 0;
    uint8_t itemIndex = 0;
    double value = sum;
-   bool storeNumDots = maxItems == 1 || this->mode == GRAPH2_METERMODE;
    GraphDataCell* itemStart = &valueStart[isPercentChart ? 0 : 1];
    while (true) {
       numDots = 0;
