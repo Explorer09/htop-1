b87540f1a2dc33a99bf7e1efdb118ff6dcf10152
Explorer09 <explorer09@gmail.com>
2025-09-27 17:33:43 +0800

    wd13b (disasm)

--- Meter.c
+++ Meter.c
@@ -1260,7 +1260,7 @@ static void GraphMeterMode_draw(Meter* this, int x, int y, int w) {
    }
 
    bool isPercentChart = Meter_isPercentChart(this);
-   bool inNumDots = maxItems == 1 || this->mode == GRAPH2_METERMODE;
+   bool inNumDots = this->mode == GRAPH2_METERMODE || maxItems == 1;
 
    size_t nCellsPerValue = inNumDots ? maxItems : h;
    if (!isPercentChart) {
