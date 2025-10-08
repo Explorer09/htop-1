af6ce6a43ddc1b9c042f53be527b5e7571ff4c74
Explorer09 <explorer09@gmail.com>
2025-10-08 17:04:42 +0800

    Rework Vector_softRemove() and compact() operations
    Remove the 'dirty_index' and 'dirty_count' members of Vector object.
    Let the caller of the Vector_softRemove() operation track the
    'dirtyIndex' and pass it to Vector_compact() when needed.
    'dirtyIndex' is supposed to have shorter lifespan than a Vector
    instance, and by removing the member we can save object size.
    
    'dirty_count' is redundant and thus removed as well.
    A boolean 'isDirty' member is added to a Vector object for ease of
    debugging.
    
    Signed-off-by: Kang-Che Sung <explorer09@gmail.com>

--- Vector.h
+++ Vector.h
@@ -22,11 +22,8 @@ typedef struct Vector_ {
    int arraySize;
    int growthRate;
    int items;
-   /* lowest index of a pending soft remove/delete operation,
-      used to speed up compaction */
-   int dirty_index;
-   /* count of soft deletes, required for Vector_count to work in debug mode */
-   int dirty_count;
+
+   bool isDirty;
    bool owner;
 } Vector;
 
@@ -56,7 +53,7 @@ Object* Vector_remove(Vector* this, int idx);
 Object* Vector_softRemove(Vector* this, int idx);
 
 /* Vector_compact reclaims space free'd up by Vector_softRemove, if any. */
-void Vector_compact(Vector* this);
+void Vector_compact(Vector* this, int dirtyIndex);
 
 void Vector_moveUp(Vector* this, int idx);
 
